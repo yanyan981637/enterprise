5ed538b690ea845e94508b2f3181d59336a7fd96
arvin <arvin.duan@mic.com.tw>
2025-02-19 10:14:27 +0800

    remove modules: Mitac_Homepage Mitac_SystemAPI Mitac_Community Mitac_CustomCatalog Mitac_CustomCMS Mitac_CustomNinja

--- app/code/Mitac/Community/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-	\Magento\Framework\Component\ComponentRegistrar::register(
-		\Magento\Framework\Component\ComponentRegistrar::MODULE,
-		'Mitac_Community',
-		__DIR__
-	);
--- app/code/Mitac/CustomCatalog/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Mitac_CustomCatalog',
-	__DIR__
-);
